34def695764e94a0852ff31fbd31c160489e4dec
Relentless <relentless@rlnt.dev>
2022-04-09 02:30:18 +0200

    add number and energy formatting to text utils

--- src/main/java/com/github/almostreliable/lazierae2/util/TextUtil.java
+++ src/main/java/com/github/almostreliable/lazierae2/util/TextUtil.java
@@ -6,9 +6,18 @@ import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 import static com.github.almostreliable.lazierae2.core.Constants.MOD_ID;
 
 public final class TextUtil {
+
+    private static final Locale LOCALE = Locale.getDefault();
+    private static final DecimalFormat DF = (DecimalFormat) NumberFormat.getInstance(LOCALE).clone();
+    private static final String[] UNITS = {"", "k", "M", "G", "T", "P"};
 
     private TextUtil() {}
 
@@ -57,6 +66,44 @@ public final class TextUtil {
      */
     public static StringTextComponent colorize(String input, TextFormatting color) {
         return (StringTextComponent) new StringTextComponent(input).withStyle(color);
+    }
+
+    /**
+     * Formats a number into a human-readable energy string.
+     *
+     * @param energy    The energy to format
+     * @param minPlaces The minimum number of decimal places
+     * @param maxPlaces The maximum number of decimal places
+     * @param extended  Whether to use extended formatting
+     * @param suffix    Whether to use the energy suffix like FE or kFE
+     * @return The formatted energy string
+     */
+    public static String formatEnergy(Number energy, int minPlaces, int maxPlaces, boolean extended, boolean suffix) {
+        // extended format
+        if (extended) return formatNumber(energy, minPlaces, maxPlaces) + (suffix ? " FE" : "");
+        // compact format
+        int numberOfDigits = energy.intValue() == 0 ? 0 :
+            (int) (1 + Math.floor(Math.log10(Math.abs(energy.doubleValue()))));
+        int base10Exponent = numberOfDigits < 4 ? 0 : 3 * ((numberOfDigits - 1) / 3);
+        double normalized = energy.doubleValue() / Math.pow(10, base10Exponent);
+        return formatNumber(normalized, minPlaces, maxPlaces) + (suffix ? " " + UNITS[base10Exponent / 3] + "FE" : "");
+    }
+
+    /**
+     * Formats a number into a correctly rounded string with the given number of decimal places.
+     * <p>
+     * The method uses the locale of the current user and formats the number accordingly.
+     *
+     * @param number    the number to format
+     * @param minPlaces the minimum number of decimal places
+     * @param maxPlaces the maximum amount of decimal places
+     * @return the formatted number
+     */
+    public static String formatNumber(Number number, int minPlaces, int maxPlaces) {
+        DF.setRoundingMode(RoundingMode.HALF_UP);
+        DF.setMinimumFractionDigits(minPlaces);
+        DF.setMaximumFractionDigits(maxPlaces);
+        return DF.format(number);
     }
 
     /**
