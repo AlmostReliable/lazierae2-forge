99251df78c46f1e4ba0bdb7ec9677f355d174eb5
Relentless <relentless@rlnt.dev>
2022-04-19 20:58:23 +0200

    refactor shift clicking logic

--- src/main/java/com/almostreliable/lazierae2/machine/MachineContainer.java
+++ src/main/java/com/almostreliable/lazierae2/machine/MachineContainer.java
@@ -22,9 +22,6 @@ import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 import net.minecraftforge.items.wrapper.InvWrapper;
 
-import javax.annotation.Nullable;
-import java.util.stream.IntStream;
-
 import static com.almostreliable.lazierae2.util.TextUtil.f;
 
 public class MachineContainer extends Container {
@@ -44,38 +41,35 @@ public class MachineContainer extends Container {
         syncData();
     }
 
-    @SuppressWarnings("java:S3776")
     @Override
     public ItemStack quickMoveStack(PlayerEntity player, int index) {
-        // this should be refactored, but I'm too lazy to do that for 1.16
         ItemStack stack = ItemStack.EMPTY;
         Slot slot = slots.get(index);
 
-        // check if the slot exists and has an item inside
-        if (slot == null || !slot.hasItem()) return stack;
+        // check if the slot has an item inside
+        if (!slot.hasItem()) return stack;
 
         ItemStack slotStack = slot.getItem();
         stack = slotStack.copy();
 
-        // decide where to put the item
         if (index < inventory.getSlots()) {
-            // transfer item from machine to inventory
+            // from machine to inventory
             if (!moveItemStackTo(slotStack, inventory.getSlots(), inventory.getSlots() + PLAYER_INV_SIZE, false)) {
                 return ItemStack.EMPTY;
             }
         } else if (GameUtil.isValidUpgrade(slotStack)) {
-            // transfer item from inventory to upgrade slot
-            if (!moveItemStackTo(slotStack, 0, 1, false)) return ItemStack.EMPTY;
+            // from inventory to upgrade slot
+            if (!moveItemStackTo(slotStack, InventoryHandler.UPGRADE_SLOT, InventoryHandler.UPGRADE_SLOT + 1, false)) {
+                return ItemStack.EMPTY;
+            }
         } else {
-            // transfer item from inventory to machine input slots
-            Slot inputSlot = inputsContainItem(stack);
-            if (slotStack.isStackable() && inputSlot != null && (inputSlot.getItem().getCount() <
-                Math.min(inputSlot.getMaxStackSize(), inputSlot.getItem().getMaxStackSize()))) {
-                // merge item stack if one input slot has the same item already
-                mergeItemStackTo(stack, inputSlot);
-            } else {
-                // transfer item stack if there are empty inventory slots
-                if (!moveItemStackTo(slotStack, 2, inventory.getSlots(), false)) return ItemStack.EMPTY;
+            // from inventory to machine inputs
+            if (!moveItemStackTo(slotStack,
+                InventoryHandler.NON_INPUT_SLOTS,
+                InventoryHandler.NON_INPUT_SLOTS + inventory.getSlots(),
+                false
+            )) {
+                return ItemStack.EMPTY;
             }
         }
 
@@ -83,7 +77,6 @@ public class MachineContainer extends Container {
         if (slotStack.isEmpty()) {
             slot.set(ItemStack.EMPTY);
         } else {
-            // call this so the tile entity is marked as changed and saved
             slot.setChanged();
         }
 
@@ -149,46 +142,6 @@ public class MachineContainer extends Container {
         // hot bar
         for (int i = 0; i < 9; i++) {
             addSlot(new SlotItemHandler(inventory, i, 8 + i * 18, 130));
-        }
-    }
-
-    /**
-     * Checks if the input slots of the inventory contain the given item.
-     *
-     * @param stack the item to check for
-     * @return the slot containing the item, or null if not found
-     */
-    @Nullable
-    private Slot inputsContainItem(ItemStack stack) {
-        return IntStream
-            .range(InventoryHandler.NON_INPUT_SLOTS, inventory.getInputSlots() + InventoryHandler.NON_INPUT_SLOTS)
-            .mapToObj(slots::get)
-            .filter(slot -> Container.consideredTheSameItem(slot.getItem(), stack))
-            .findFirst()
-            .orElse(null);
-    }
-
-    /**
-     * Tries to merge the given item stack to the given slot.
-     * <p>
-     * If the slot changed data, the slot is marked as changed and the tile entity is saved.
-     *
-     * @param stack the item stack to merge
-     * @param slot  the slot to merge the item stack to
-     */
-    private void mergeItemStackTo(ItemStack stack, Slot slot) {
-        ItemStack slotStack = slot.getItem();
-
-        int mergedAmount = slotStack.getCount() + stack.getCount();
-        int maxSize = Math.min(slot.getMaxStackSize(), stack.getMaxStackSize());
-        if (mergedAmount <= maxSize) {
-            stack.setCount(0);
-            slotStack.setCount(mergedAmount);
-            slot.setChanged();
-        } else if (slotStack.getCount() < maxSize) {
-            stack.shrink(maxSize - slotStack.getCount());
-            slotStack.setCount(maxSize);
-            slot.setChanged();
         }
     }
 
