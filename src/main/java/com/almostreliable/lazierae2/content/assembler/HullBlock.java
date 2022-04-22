14c0b50428edf0fb06a621b80f2a794a322bf501
Relentless <relentless@rlnt.dev>
2022-04-23 00:51:22 +0200

    move optional direction property to hull block

--- src/main/java/com/almostreliable/lazierae2/content/assembler/HullBlock.java
+++ src/main/java/com/almostreliable/lazierae2/content/assembler/HullBlock.java
@@ -1,7 +1,11 @@
 package com.almostreliable.lazierae2.content.assembler;
 
 import com.almostreliable.lazierae2.core.TypeEnums.HULL_TYPE;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.util.Tuple;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -11,14 +15,19 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.phys.BlockHitResult;
 
 import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
 
 public class HullBlock extends AssemblerBlock {
 
-    public static final OptionalDirectionProperty HORIZONTAL = OptionalDirectionProperty.HORIZONTAL;
-    public static final OptionalDirectionProperty VERTICAL = OptionalDirectionProperty.VERTICAL;
+    public static final OptionalDirectionProperty HORIZONTAL = OptionalDirectionProperty.HORIZONTAL_PROP;
+    public static final OptionalDirectionProperty VERTICAL = OptionalDirectionProperty.VERTICAL_PROP;
     public final HULL_TYPE type;
 
     public HullBlock(HULL_TYPE type) {
@@ -143,5 +152,94 @@ public class HullBlock extends AssemblerBlock {
         }
 
         return OptionalDirection.WEST;
+    }
+
+    private enum OptionalDirection implements StringRepresentable {
+
+        NONE("none", null),
+        UP("up", Direction.UP),
+        DOWN("down", Direction.DOWN),
+        NORTH("north", Direction.NORTH),
+        EAST("east", Direction.EAST),
+        SOUTH("south", Direction.SOUTH),
+        WEST("west", Direction.WEST);
+
+        private static final BiMap<OptionalDirection, Direction> OPT_TO_DIR = HashBiMap.create();
+        @Nullable
+        private final Direction direction;
+        private final String name;
+
+        static {
+            for (var value : values()) {
+                OPT_TO_DIR.put(value, value.direction);
+            }
+        }
+
+        OptionalDirection(String name, @Nullable Direction direction) {
+            this.name = name;
+            this.direction = direction;
+        }
+
+        @Nullable
+        public static OptionalDirection fromDirection(@Nullable Direction direction) {
+            return OPT_TO_DIR.inverse().get(direction);
+        }
+
+        @Nullable
+        public static Direction fromOptionalDirection(@Nullable OptionalDirection direction) {
+            return OPT_TO_DIR.get(direction);
+        }
+
+        public BlockPos relative(BlockPos pos) {
+            if (direction == null) {
+                return pos;
+            }
+
+            return pos.relative(direction);
+        }
+
+        public void relative(BlockPos.MutableBlockPos pos) {
+            if (direction != null) {
+                pos.move(direction.getNormal());
+            }
+        }
+
+        @Nullable
+        public Direction getDirection() {
+            return direction;
+        }
+
+        @Override
+        public String getSerializedName() {
+            return name;
+        }
+    }
+
+    private static final class OptionalDirectionProperty extends EnumProperty<OptionalDirection> {
+
+        private static final OptionalDirectionProperty HORIZONTAL_PROP = create("horizontal",
+            Direction.NORTH,
+            Direction.EAST,
+            Direction.SOUTH,
+            Direction.WEST
+        );
+        private static final OptionalDirectionProperty VERTICAL_PROP = create("vertical", Direction.UP, Direction.DOWN);
+
+        private OptionalDirectionProperty(
+            String name, Collection<Direction> directions
+        ) {
+            super(name, OptionalDirection.class, createOptionalDirections(directions));
+        }
+
+        private static OptionalDirectionProperty create(String name, Direction... directions) {
+            return new OptionalDirectionProperty(name, List.of(directions));
+        }
+
+        private static Collection<OptionalDirection> createOptionalDirections(Collection<Direction> directions) {
+            Set<OptionalDirection> optionalDirections = EnumSet.noneOf(OptionalDirection.class);
+            optionalDirections.add(OptionalDirection.NONE);
+            directions.forEach(direction -> optionalDirections.add(OptionalDirection.fromDirection(direction)));
+            return optionalDirections;
+        }
     }
 }
