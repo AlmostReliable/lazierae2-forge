d6c42d742066ce01225812d36be0607b61e701dd
Relentless <relentless@rlnt.dev>
2022-04-23 00:57:26 +0200

    extract screen opening logic to generic block

--- src/main/java/com/almostreliable/lazierae2/content/GenericBlock.java
+++ src/main/java/com/almostreliable/lazierae2/content/GenericBlock.java
@@ -4,17 +4,24 @@ import com.almostreliable.lazierae2.core.TypeEnums.TRANSLATE_TYPE;
 import com.almostreliable.lazierae2.util.GuiUtil.Tooltip;
 import com.almostreliable.lazierae2.util.TextUtil;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.Material;
+import net.minecraftforge.network.NetworkHooks;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -53,6 +60,17 @@ public abstract class GenericBlock extends Block {
             tooltip.addAll(Tooltip.builder().line(f("{}.description", getId()), ChatFormatting.AQUA).build());
         }
         super.appendHoverText(stack, level, tooltip, flag);
+    }
+
+    protected InteractionResult openScreen(
+        Level level, BlockPos pos, Player player
+    ) {
+        if (level.isClientSide() || player.isShiftKeyDown()) return InteractionResult.SUCCESS;
+        var entity = level.getBlockEntity(pos);
+        if (entity instanceof MenuProvider menuProvider && player instanceof ServerPlayer invoker) {
+            NetworkHooks.openGui(invoker, menuProvider, pos);
+        }
+        return InteractionResult.CONSUME;
     }
 
     public String getId() {
--- src/main/java/com/almostreliable/lazierae2/content/MachineBlock.java
+++ src/main/java/com/almostreliable/lazierae2/content/MachineBlock.java
@@ -2,10 +2,8 @@ package com.almostreliable.lazierae2.content;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
@@ -16,7 +14,6 @@ import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraftforge.network.NetworkHooks;
 
 import javax.annotation.Nullable;
 
@@ -47,11 +44,6 @@ public abstract class MachineBlock extends GenericBlock implements EntityBlock {
     public InteractionResult use(
         BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit
     ) {
-        if (level.isClientSide() || player.isShiftKeyDown()) return InteractionResult.SUCCESS;
-        var entity = level.getBlockEntity(pos);
-        if (entity instanceof MenuProvider menuProvider && player instanceof ServerPlayer invoker) {
-            NetworkHooks.openGui(invoker, menuProvider, pos);
-        }
-        return InteractionResult.CONSUME;
+        return openScreen(level, pos, player);
     }
 }
