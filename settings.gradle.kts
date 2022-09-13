d738534d2b64dab12d6dabc5ee6c12ef2fb8bd82
Relentless <relentless@rlnt.dev>
2022-09-13 15:20:14 +0200

    switch to loom and kotlin dsl

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.architectury.dev/")
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.minecraftforge.net/")
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LazierAE2"
